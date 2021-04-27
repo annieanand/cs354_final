d84a3f04de0215906e5ed53f66c62e22068b0b16
Annie Anand <anagata.anand@gmail.com>
2021-04-27 17:36:22 -0500

    able to display a blue window

---
+++ main.py
@@ -0,0 +1,15 @@
+from p5 import *
+import numpy as np
+
+width = 1000
+height = 1000
+
+#sets up canvas to run once with above width and height
+def setup():
+    size(width, height) 
+
+#draw the background
+def draw():
+    background(30, 30, 47)
+
+run()
