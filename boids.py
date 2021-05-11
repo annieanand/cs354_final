bb1aab76e067e600ee640bb19d25a5b611c8f5eb
Annie Anand <anagata.anand@gmail.com>
2021-05-11 10:52:46 -0500

    final version

--- boids.py
+++ boids.py
@@ -9,19 +9,24 @@ class Boid():
         self.velocity = Vector(*v) #vector is built in
         v =  ((np.random.rand() - 0.5)/2, (np.random.rand() - 0.5)/2) #btwn -.25 and .25
         self.acceleration = Vector(*v) 
-
+        self.max_see_ahead = 2
+        self.max_avoid_force = 5
         self.width = width
         self.height = height
-        self.max_speed = 7
+        self.max_speed = 10
         self.max_force = 1
-        self.distance_to_next = 100
+        self.distance_to_next = 200
+       
+        self.avoidance_force = self.position + (self.velocity/np.linalg.norm(self.velocity)) * self.max_see_ahead
+
     def show(self):
         stroke(255)
-        circle((self.position.x, self.position.y), 10) #change later?
-  
+        circle((self.position.x, self.position.y), 5) #circle with a radius of 5
+        
     def update(self, is_pressed):
         if is_pressed == True:
             return
+        
         self.position = self.position + self.velocity #update position by adding the direction vector
         self.velocity = self.velocity + self.acceleration
 
@@ -30,20 +35,30 @@ class Boid():
             #normalize vectors
             self.velocity = self.velocity / np.linalg.norm(self.velocity) * self.max_speed
         self.acceleration = Vector(np.zeros(1), np.zeros(1))
+    
     #create boundaries for boids
     def bounding(self):
-        if self.position.x > self.width:
-            self.position.x = 0 
-        elif self.position.x < 0: 
-            self.position.x = self.width
-        
-        if self.position.y > self.height:
-            self.position.y = 0
-        elif self.position.y < 0:
-            self.position.y = self.height
+        if self.position.x >= self.width: #checks if the x position is greater than width
+            self.position.x = self.width-50 #resets position to move left and changes velocity to be faster and go the opp way
+            self.velocity *= -5
+            
+        elif self.position.x <= 0: #checks if x position is less than 0
+            
+            self.position.x = 50
+            self.velocity*= -5
+        if self.position.y >= self.height: #checks if y position is above height
+            
+            self.position.y = self.height-50
+            self.velocity *= -5
+        elif self.position.y <= 0: #checks if y position is below 0
+            
+            self.position.y = 50
+            self.velocity *= -5
+         
    
     #now to add behavior to the flock
-    #steering = avg_vec of boids - self.velocity
+    #steering = avg_vec of boids - self.velocity 
+    #helps with velocity matching
 
     def align(self, boids):
         steer = Vector(np.zeros(1), np.zeros(1)) #initialize steering vector
@@ -83,6 +98,7 @@ class Boid():
         return steer
     
     #last step, separation
+    #avoid getting too close to others
     def separation(self,boids):
         steer = Vector(np.zeros(1), np.zeros(1))
         total = 0
@@ -91,7 +107,8 @@ class Boid():
             dist = np.linalg.norm(b.position - self.position) #normalize vector between next boid (direction of escape)
             if self.position != b.position and dist < self.distance_to_next: #check whether the current boid is the same as the next position, and if it is within appropriate radius
                 distance_away = self.position - b.position #calculate distance away 
-                distance_away = distance_away/dist #change the distance - based on the direcction of escape                avg_dir += distance_away #append to avg direction vec
+                distance_away = distance_away/dist #change the distance - based on the direcction of escape                
+                avg_dir += distance_away #append to avg direction vec
                 total = total +1
             if total > 0:
                 avg_dir = avg_dir / total #set new average direction
@@ -102,14 +119,73 @@ class Boid():
                 if np.linalg.norm(steer) > self.max_force: #check if the steer magnitude is over the max force. if it is:
                     steer = (steer / np.linalg.norm(steer)) *self.max_force #weight the steer by force (based on dist away)
         return steer
+
+    #avoid running into boundaries 
+    def line_collision_detect(self,boids):
+        
+
+        #the ahead vector represents the perception of the boid "ahead"
+        ahead = self.position + self.velocity * self.max_see_ahead
+        criteria = False
+        
+        if abs(ahead.x - self.width/2) < 20: # set threshold to 20 ; checks whether it is about 20 away from the middle line
+        
+
+            self.avoidance_force =  self.avoidance_force  + (ahead - Vector(self.width/2, ahead.y)) # avoidance force is added to the difference btwn ahead and the obstacle x val
+            self.avoidance_force = (self.avoidance_force/np.linalg.norm(self.avoidance_force)) * self.max_avoid_force #normalize
+            self.velocity = self.velocity + self.avoidance_force
+            self.position = self.position + self.velocity
+            criteria = True
+        
+        if (ahead.x) <= 50:
+            self.avoidance_force = self.avoidance_force  + (ahead - Vector(0.0, ahead.y))
+            self.avoidance_force = (self.avoidance_force/np.linalg.norm(self.avoidance_force)) * self.max_avoid_force 
+
+            self.velocity = self.velocity + self.avoidance_force
+            self.position = self.position + self.velocity
+            criteria = True
+ 
+
+       
+        if (ahead.x) >= self.width -50:
+            self.avoidance_force = self.avoidance_force  + (ahead - Vector(self.width -30, ahead.y))
+            self.avoidance_force = (self.avoidance_force/np.linalg.norm(self.avoidance_force)) * self.max_avoid_force 
+
+            self.velocity = self.velocity + self.avoidance_force
+            self.position = self.position + self.velocity   
+            criteria = True
+           
+        if (ahead.y <= 50):
+            self.avoidance_force = self.avoidance_force  + (ahead - Vector(ahead.x,0.0))
+            self.avoidance_force = (self.avoidance_force/np.linalg.norm(self.avoidance_force)) * self.max_avoid_force 
+
+            self.velocity = self.velocity + self.avoidance_force
+            self.position = self.position + self.velocity 
+            criteria = True
+        if (ahead.y >= self.height-50):
+            self.avoidance_force = self.avoidance_force  + (ahead - Vector(ahead.x, self.height-30))
+            self.avoidance_force = (self.avoidance_force/np.linalg.norm(self.avoidance_force)) * self.max_avoid_force 
+            self.velocity = self.velocity + self.avoidance_force
+            self.position = self.position + self.velocity 
+            criteria = True
+        
+        if criteria:
+            return self.avoidance_force
+        else:
+            return Vector(0.0,0.0)
+        
     
     #function to apply the different behaviors on the boids
     def apply_behavior(self, boids):
         aligned = self.align(boids)
-        self.acceleration += aligned
+        self.acceleration += aligned *0.25
 
         cohe =  self.cohesion(boids)
-        self.acceleration += cohe
+        self.acceleration += cohe * 0.75
 
         separation = self.separation(boids)
-        self.acceleration += separation
+        self.acceleration += separation * 0.75
+
+        #apply the avoidance
+        avoidance = self.line_collision_detect(boids)
+        self.acceleration += avoidance*.75
--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 from p5 import *
 import numpy as np
 from boids import Boid
-
+ 
 width = 1000
-height = 1000
+height = 800
 flock = []
+
 #create the flock (random points) - 10 
-for i in range(0,10):
+for i in range(0,15):
     flock.append(Boid(np.random.rand()*1000, np.random.rand()*1000,width, height))
+
 #sets up canvas to run once with above width and height
 def setup():
     size(width, height) 
 
 #draw the background
 def draw():
-    is_pressed = False
+    m_is_pressed = False
+    k_is_pressed = False
     background(30, 30, 47)
+    line((500,0), (500,800))#create the obstacle as a line from midway through width with a length up and down on the window
     if mouse_is_pressed:
-        is_pressed = True
+        m_is_pressed = True
+    if key_is_pressed:
+        if str(key) == "a":
+           flock.append(Boid(np.random.rand()*1000, np.random.rand()*1000,width, height)) 
+           k_is_pressed = True
     #loop through the flock array to show the boid
     for b in flock:
         b.bounding()
         b.show()
         b.apply_behavior(flock)
-        b.update(is_pressed)    
+        b.update(m_is_pressed)    
 
 
 if __name__ == '__main__':
